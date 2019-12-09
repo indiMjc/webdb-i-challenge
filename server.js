381197d752fc585e4397609942c6282e2743d1b8
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-09 16:12:11 -0500

    server up

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  const message = "Server up";
+  res.send(message);
+});
 
 module.exports = server;
